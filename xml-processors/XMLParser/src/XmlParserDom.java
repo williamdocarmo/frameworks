70038d8c59c6ac6860db3fb95a1159fb541b3a8f
santanubh89 <santanu.mckv@gmail.com>
2019-09-21 18:24:17 +0530

    Redis added

---
+++ xml-processors/XMLParser/src/XmlParserDom.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class XmlParserDom {
+	
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse("Request.xml");
+		
+		// Get Root Element
+		Element element = document.getDocumentElement();
+	}
+
+}
