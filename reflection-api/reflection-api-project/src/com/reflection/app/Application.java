4254b5f989e6c6d6974268098ae5ae9dba362db3
santanubh89 <santanu.mckv@gmail.com>
2020-03-29 01:28:23 +0530

    Added Reflection API Project

---
+++ reflection-api/reflection-api-project/src/com/reflection/app/Application.java
@@ -0,0 +1,114 @@
+package com.reflection.app;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.reflection.app.vo.Employee;
+
+public class Application {
+	
+	private static String defaultString = "DefaultValue";
+	private static int defaultInt = 30;
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IllegalArgumentException, InvocationTargetException {
+		// instantiateClassMethod1();
+		// analyzeClassConstructors();
+		// analyzeClassVariables();
+		// getSetObjectValuesUsingVariables();
+		// analyzeClassMethods();
+		// invokeClassMethods();
+		analyzeClassAnnotations();
+	}
+
+	private static void instantiateClassMethod1() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		Employee employee1 = Employee.class.newInstance();
+		employee1.setEmployeeId("Emp1").setEmployeeName("Fname1 Lname1").setAge(30).setDesignation("Senior Developer").setAddress(new ArrayList<String>());
+		System.out.println("Employee: " + employee1);
+		Employee employee2 = (Employee) Class.forName("com.reflection.app.vo.Employee").newInstance();
+		System.out.println("Employee: " + employee2);
+	}
+
+	private static void analyzeClassConstructors() throws InstantiationException, IllegalAccessException, ClassNotFoundException, IllegalArgumentException, InvocationTargetException {
+		Class clazz = Employee.class;
+		Object object = null;
+		Constructor[] constructors = clazz.getConstructors();
+		for (Constructor constructor : constructors) {
+			if (constructor.getParameterCount() > 0) {
+				object = constructor.newInstance("Emp3", "Fname3 Lname3", 35, "Solution Architect", null);
+			}
+		}
+		System.out.println(object);
+	}
+	
+	private static void analyzeClassVariables() throws IllegalArgumentException, IllegalAccessException {
+		Class clazz = Employee.class;
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			System.out.println("Field: "+field.getName()+" "+field.getType());
+		}
+	}
+	
+	private static void getSetObjectValuesUsingVariables() throws IllegalArgumentException, IllegalAccessException {
+		Employee employee = new Employee();
+		Class clazz = employee.getClass();
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			field.setAccessible(true);
+			if (field.getType().equals(String.class)) {
+				field.set(employee, defaultString);
+			}
+			if (field.getType().equals(int.class)) {
+				field.setInt(employee, defaultInt);
+			}
+		}
+		System.out.println("Employee: "+employee);
+		for (Field field : fields) {
+			System.out.println("Field: "+field.getName()+" :: Value: "+field.get(employee));
+		}
+	}
+	
+	private static void analyzeClassMethods() throws IllegalArgumentException, IllegalAccessException {
+		Class clazz = Employee.class;
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			System.out.println("Method: "+method.getName()+" "+method.getReturnType());
+		}
+	}
+	
+	private static void invokeClassMethods() throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		Employee employee = new Employee();
+		Class clazz = employee.getClass();
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			if (method.getName().startsWith("set")) {
+				Parameter[] parameters = method.getParameters();
+				for (Parameter parameter : parameters) {
+					if (parameter.getType().equals(String.class)) {
+						method.invoke(employee, defaultString);
+					}
+					if (parameter.getType().equals(int.class)) {
+						method.invoke(employee, defaultInt);
+					}
+				}
+			}
+		}
+		System.out.println("Employee: "+employee);
+	}
+	
+	private static void analyzeClassAnnotations() throws IllegalArgumentException, IllegalAccessException {
+		Class clazz = Employee.class;
+		XmlRootElement xmlRoot = (XmlRootElement) clazz.getAnnotation(XmlRootElement.class);
+		System.out.println("XML Root Element: "+xmlRoot.name()+" "+xmlRoot.namespace());
+		WebService webService = (WebService) clazz.getAnnotation(WebService.class);
+		System.out.println("Web Service: "+webService.portName()+" "+webService.serviceName());
+	}
+
+}
---
+++ reflection-api/reflection-api-project/src/com/reflection/app/vo/Employee.java
@@ -0,0 +1,87 @@
+package com.reflection.app.vo;
+
+import java.util.List;
+
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement(name = "empDetails", namespace = "http://com/reflection/app")
+@WebService(portName = "employeePort", serviceName = "employeeService")
+public class Employee {
+	
+	private String employeeId;
+	
+	private String employeeName;
+	
+	private int age;
+	
+	private String designation;
+	
+	private List<String> address;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public Employee setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+		return this;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public Employee setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+		return this;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Employee setAge(int age) {
+		this.age = age;
+		return this;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public Employee setDesignation(String designation) {
+		this.designation = designation;
+		return this;
+	}
+
+	public List<String> getAddress() {
+		return address;
+	}
+
+	public Employee setAddress(List<String> address) {
+		this.address = address;
+		return this;
+	}
+
+	public Employee(String employeeId, String employeeName, int age, String designation, List<String> address) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.age = age;
+		this.designation = designation;
+		this.address = address;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", age=" + age
+				+ ", designation=" + designation + ", address=" + address + "]";
+	}
+	
+}
