81969f7a66ea9608200010daa935c1081b8853c5
santanubh89 <santanu.mckv@gmail.com>
2020-03-30 00:15:53 +0530

    Updated Junit4/Mockito Project

--- testing-frameworks/junit4-mockito/src/main/java/com/app/main/Application.java
+++ testing-frameworks/junit4-mockito/src/main/java/com/app/main/Application.java
@@ -8,7 +8,7 @@ public class Application {
 	public String execute(Document request) throws IllegalAccessException {
 		String operation = request.getDocumentElement().getNodeName();
 		String msisdn = request.getFirstChild().getFirstChild().getNextSibling().getTextContent();
-		String command = operation+":MSISDN="+msisdn+";";
+		String command = operation.replace("Subscriber", "").toUpperCase()+":MSISDN="+msisdn+";";
 		String response = ServerHandler.getInstance().getServerContext().executeServerOperation(command);
 		return response;
 		
--- testing-frameworks/junit4-mockito/src/test/java/com/app/main/TestFramework.java
+++ testing-frameworks/junit4-mockito/src/test/java/com/app/main/TestFramework.java
@@ -1,6 +1,6 @@
 package com.app.main;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -37,7 +37,6 @@ public class TestFramework {
 			public String answer(InvocationOnMock invocation) throws Throwable {
 				String request = (String) invocation.getArguments()[0];
 				String command = request.split(":")[0];
-				System.out.println("Command: " +command);
 				return responseMap.get(command);
 			}
 		});
@@ -51,20 +50,30 @@ public class TestFramework {
 	
 	private void populateResponseMap() {
 		responseMap = new HashMap<String, String>();
-		responseMap.put("CreateSubscriber", "CREATE:SUCCESSFUL");
-		responseMap.put("DeleteSubscriber", "DELETE:SUCCESSFUL");
-		responseMap.put("GetSubscriber", "GET:SUCCESSFUL");
-		
+		responseMap.put("CREATE", "CREATE:SUCCESSFUL");
+		responseMap.put("DELETE", "DELETE:SUCCESSFUL");
+		responseMap.put("GET", "GET:SUCCESSFUL");
 	}
 
 	public void executeTestcase(String testcaseName) throws SAXException, IOException, ParserConfigurationException, IllegalAccessException {
 		String requestFile = requestMap.get(testcaseName);
 		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("src/test/resources/"+requestFile);
-		String response = applicationInstance.execute(document);
-		String expectedResponse = responseMap.get(document.getDocumentElement().getNodeName());
-		System.out.println("Actual Response: "+response);
-		System.out.println("Expected Response: "+expectedResponse);
-		assertEquals(expectedResponse, response);
+		String response = "";
+		boolean isException = false;
+		try{
+			response = applicationInstance.execute(document);
+			System.out.println("Response "+response);
+		} catch (Exception e) {
+			isException = true;
+		}
+		if (testcaseName.endsWith("Failed")) {
+			assertTrue(isException);
+		} else {
+			assertTrue(response.contains("SUCCESSFUL"));
+		}
+	}
+	
+	public void analyzeMockito() {
 	}
 
 }
