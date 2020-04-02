c4f61b23a3c06ed994a27b2e8de64dc8a00570a1
Santanu Bhattacharyya <santanu.bhattacharyya@ericsson.com>
2020-04-02 17:47:04 +0530

    Added XJC Project Renamed JAXB Project

---
+++ xml-processors/jaxb-illustration/src/com/jaxb/app/JAXBApplication.java
@@ -0,0 +1,38 @@
+package com.jaxb.app;
+
+import java.io.File;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.jaxb.app.vo.Employee;
+
+public class JAXBApplication {
+	
+	public static void main(String args[]) throws JAXBException {
+		marshall();
+		
+		unmarshall();
+	}
+	
+	private static void marshall() throws JAXBException {
+		Object object = ObjectMaker.makeObject();
+		StringWriter writer = new StringWriter();
+		JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.marshal(object, writer);
+		System.out.println(writer.toString());
+	}
+
+	private static void unmarshall() throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		Object object = unmarshaller.unmarshal(new File("Request.xml"));
+		System.out.println("Object: "+object);
+	}
+	
+}
