f855a947c7aee721238c89cbc5109c3ec79c88ab
Santanu Bhattacharyya <santanu.bh6@gmail.com>
2021-04-04 19:15:32 +0530

    config files added

--- spring-boot/config-server/spring-boot-cloud-config/src/main/java/com/config/springbootconfig/GreetingController.java
+++ spring-boot/config-server/spring-boot-cloud-config/src/main/java/com/config/springbootconfig/GreetingController.java
@@ -1,34 +1,23 @@
 package com.config.springbootconfig;
 
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
-	// Default value
-	@Value("${greeting.message: Default Hello World}")
-	private String greetingMessage;
-	
-	// List values
-	@Value("${my.list.values}")
-	private List<String> listValues;
-	
 	@Autowired
 	private DBSettings dbSettings;
 
-	@RequestMapping("/{user}")
-	public String greetUser(@PathVariable(name = "user") String username) {
+	@RequestMapping("/greeting")
+	public String greetUser() {
 		String message = "";
 		message += dbSettings.getConnection()+ " "+dbSettings.getHost()+" "+dbSettings.getPort()+ "<br />";
 		return message;
